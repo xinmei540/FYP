f8ff6cdffd07b76ab2d1f79a0195d6b4d5276609
Xin Xie <D17128351@mydit.ie>
2022-03-28 08:11:59 +0800

    FYP

--- Final Year Project/Assets/Script/Sign.cs
+++ Final Year Project/Assets/Script/Sign.cs
@@ -3,14 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Sign : MonoBehaviour
+public class Sign : Interactable
 {
-    public SignalSent contextOn;
-    public SignalSent contextOff;
     public GameObject dialogBox;
     public Text dialogText;
     public string dialog;
-    public bool playerInRange;
 
     // Start is called before the first frame update
     void Start()
@@ -35,20 +32,11 @@ public class Sign : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if(other.CompareTag("Player"))
-        {
-            contextOn.Raise();
-            playerInRange = true;
-        }
-    }
-
     private void OnTriggerExit2D(Collider2D other)
     {
-        if(other.CompareTag("Player"))
+        if(other.CompareTag("Player") && !other.isTrigger)
         {
-            contextOff.Raise();
+            context.Raise();
             playerInRange = false;
             dialogBox.SetActive(false);
         }
